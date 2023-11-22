a6f4ac1a8885acd616ad51b31679eacb5bd51e6c
SashaPotemkin <alex.potjomkin@gmail.com>
2023-11-22 12:04:52 +0300

    Gradle impls

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,9 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
+    implementation ("androidx.compose.material:material:1.4.2")
+    implementation ("androidx.navigation:navigation-compose:2.5.2")
+
     //Lottie
     implementation("com.airbnb.android:lottie-compose:6.0.0")
 
